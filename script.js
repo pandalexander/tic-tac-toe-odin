ff735ce4860cfe0f2f616f8db58b01e64a563a3a
Alex Seeley <alexseeley134@gmail.com>
2024-01-06 12:41:33 -0700

    Add makeSpace function to mark game board

--- script.js
+++ script.js
@@ -23,3 +23,71 @@ const playerTwo = (function () {
   const symbol = "O";
   return { name, symbol };
 })();
+
+const markSpace = (function () {
+  const topLeft = (symbol) => {
+    if (typeof gameBoard.boardMatrix[0][0] === "number") {
+      gameBoard.boardMatrix[0][0] = symbol;
+    }
+  };
+
+  const topMiddle = (symbol) => {
+    if (typeof gameBoard.boardMatrix[0][1] === "number") {
+      gameBoard.boardMatrix[0][1] = symbol;
+    }
+  };
+
+  const topRight = (symbol) => {
+    if (typeof gameBoard.boardMatrix[0][2] === "number") {
+      gameBoard.boardMatrix[0][2] = symbol;
+    }
+  };
+
+  const left = (symbol) => {
+    if (typeof gameBoard.boardMatrix[1][0] === "number") {
+      gameBoard.boardMatrix[1][0] = symbol;
+    }
+  };
+
+  const middle = (symbol) => {
+    if (typeof gameBoard.boardMatrix[1][1] === "number") {
+      gameBoard.boardMatrix[1][1] = symbol;
+    }
+  };
+
+  const right = (symbol) => {
+    if (typeof gameBoard.boardMatrix[1][2] === "number") {
+      gameBoard.boardMatrix[1][2] = symbol;
+    }
+  };
+
+  const bottomLeft = (symbol) => {
+    if (typeof gameBoard.boardMatrix[2][0] === "number") {
+      gameBoard.boardMatrix[2][0] = symbol;
+    }
+  };
+
+  const bottomMiddle = (symbol) => {
+    if (typeof gameBoard.boardMatrix[2][1] === "number") {
+      gameBoard.boardMatrix[2][1] = symbol;
+    }
+  };
+
+  const bottomRight = (symbol) => {
+    if (typeof gameBoard.boardMatrix[2][2] === "number") {
+      gameBoard.boardMatrix[2][2] = symbol;
+    }
+  };
+
+  return {
+    topLeft,
+    topMiddle,
+    topRight,
+    left,
+    middle,
+    right,
+    bottomLeft,
+    bottomMiddle,
+    bottomRight,
+  };
+})();
