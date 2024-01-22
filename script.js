1ff3c2349928d6b9ce55ec66ed8ea955566383d4
Alex Seeley <alexseeley134@gmail.com>
2024-01-21 22:57:30 -0700

    Bring in connect 4 example to start mapping
    I brought in a connect 4 coding example to help me understand
    how to build a house from inside out. I will use it to help me
    plan out the tic tac toe game so I don'd end up with a ton of problems like before.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create IIFE Gameboard that gets current state, makes mark, and prints
+// Maybe set a data key for numbering the spaces and then building the 3x3 board afterward
+// make sure that the makesMark function can only operate if the Cell()'s value is 0. If it is 1 or 2, it cannot work.
+
+// Put a function that creates an object (like the Cell() in the example) that contains a function to change it's current value to a player's token and a function is get it's current value
+
+//  Get some function that marks a square either a 0, 1, or 2. 0 means empty, 1 is player 1, 2 is player 2. Labeling token is a good idea.
+
+// Create player objects with player 1 having token = 1, player 2 having token = 2
