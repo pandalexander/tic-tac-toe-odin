c5f77b8c0ae976614608fa80bac3e47b3f2ac69a
Alex Seeley <alexseeley134@gmail.com>
2024-01-22 21:25:45 -0700

    Get Gameboard and Cell working and add comments

--- script.js
+++ script.js
@@ -1,36 +1,73 @@
-// Big Picture
-
-// Create Gameboard, get board function, add mark function (Location, player), print board to console
+// Gameboard module to manage the tic-tac-toe game state
 
 const Gameboard = (function () {
+  // Define the number of rows and columns on the game board
   const rows = 3;
   const columns = 3;
+
+  // Initialize an empty 2D array to represent the game board
   const board = [];
 
-  return { board };
-  console.log("yoooo");
+  // Loop to create rows and columns and fill the board with Cell objects
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  // Function to get the current state of the game board
+  const getBoard = () => board;
+
+  // Function to mark a cell with a player's value (1 or 2)
+  const markCell = (row, column, player) => {
+    board[row][column].setCellValue(player);
+  };
+
+  // Function to print the current state of the game board with values
+  const printBoard = () => {
+    const boardWithValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithValues);
+  };
+
+  // Function to clear the entire game board
+  const clearBoard = () => {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+        board[i][j].clearCell();
+      }
+    }
+  };
+
+  // Return the public methods for external use
+  return { getBoard, markCell, printBoard, clearBoard };
 })();
 
-// Create Cell(); that creates an object where it is instantiated. Object should have a private value attribute and an add mark method that is called inside the Gameboard's mark function. In the add mark method, if current value is 1 or 2, stop execution. Also add function to get private value variable that will be called when printing board. (get all available markable places, mark only unavailable)
-
+// Cell factory to represent individual cells on the game board
 function Cell() {
   let value = 0;
 
-  function markCell(player) {
+  function setCellValue(player) {
+    // Check if the cell is empty before setting the value
     if (!value) {
       value = player;
     }
   }
 
+  // Function to get the current value of the cell
   function getValue() {
     return value;
   }
 
+  // Function to clear the cell by resetting its value to 0
   function clearCell() {
     value = 0;
   }
 
-  return { markCell, getValue, clearCell };
+  // Return the public methods for external use
+  return { setCellValue, getValue, clearCell };
 }
 
 // Create player objects with player 1 having mark = 1, player 2 having mark = 2
